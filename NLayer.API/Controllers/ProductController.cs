cf1ff9f6d9fa8f30c6765a05a65442ff07572cf2
Yusa Tosun <yusatosun.yt@gmail.com>
2023-07-09 17:13:42 +0300

    CustomResponseDTO

---
+++ NLayer.API/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.NewFolder;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class ProductController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+
+        public ProductController(IMapper mapper, IService<Product> service)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+
+        [HttpGet(Name = "GetAll")]
+
+        public async Task<IActionResult> All()
+        {
+            var product = await _service.GetAllAsync();
+            var productDtos = _mapper.Map<List<ProductDto>>(product);
+            //return Ok(CustomResponseDto<List<ProductDto>>.Success(200, productDtos));
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productDtos));
+
+        }
+    }
+}
