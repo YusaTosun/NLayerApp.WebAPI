a203593111de4ef92785d142c037b05ff4baab91
Yusa Tosun <yusatosun.yt@gmail.com>
2023-07-11 22:32:44 +0300

    Include Methods added

---
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NLayer.API.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NLayer.Core/Repository/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using NLayer.Core.Models;
+using NLayer.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Repository
+{
+    public interface ICategoryService:IService<Category>
+    {
+    }
+}
