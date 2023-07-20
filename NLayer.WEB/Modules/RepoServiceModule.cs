5211c843e0cd1c8aea5552dd219599d487d22f10
Yusa Tosun <yusatosun.yt@gmail.com>
2023-07-20 18:04:40 +0300

    Finish

---
+++ NLayer.WEB/Modules/RepoServiceModule.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using NLayer.Caching;
+using NLayer.Core.Repository;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Repository;
+using NLayer.Repository.Repositories;
+using NLayer.Repository.UnitOfWorks;
+using NLayer.Services.Mapping;
+using NLayer.Services.Services;
+using System.Reflection;
+using Module = Autofac.Module; //todo: Ambiguous Reference hatasının önüne bu şekilde geçilebilir 
+
+namespace NLayer.WEB.Modules
+{
+    public class RepoServiceModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerLifetimeScope();
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWorks>();
+
+             var apiAssembly = Assembly.GetExecutingAssembly();
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x=>x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            //InstancePerLifetimeScope => Scope
+            //InstancePerDependency => Transient
+
+            builder.RegisterType<ProductServiceWithCaching>().As<IProductService>();
+        }
+    }
+}
